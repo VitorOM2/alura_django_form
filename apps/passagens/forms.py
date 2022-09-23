e4759567c7d1e6907880c3c80e68c661fd1e13d8
vmarques <vmarques@lliege.com.br>
2022-09-23 15:04:59 -0300

    Criado um formulário de passagens

---
+++ apps/passagens/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class PassagemForms(forms.Form):
+    """ Cria o formulário de passagens """
+    origem  = forms.CharField(label='Origem', max_length=100)
+    destino = forms.CharField(label='Destino', max_length=100)
+    ida     = forms.DateField(label='Ida')
+    volta   = forms.DateField(label='Volta')
--- setup/urls.py
+++ setup/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from apps.passagens.views import passagens
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('passagens.urls'))
