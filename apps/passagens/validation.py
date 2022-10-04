92c7477964cb9d55e04e8da8489e2d9c0abd84e5
vmarques <vmarques@lliege.com.br>
2022-10-04 14:26:39 -0300

    Alterado forma de fazer formulário

--- apps/passagens/validation.py
+++ apps/passagens/validation.py
@@ -13,9 +13,9 @@ def campo_tem_algum_numero(valor_campo, nome_campo, lista_de_erros):
 def data_de_ida_maior_que_data_volta(data_ida, data_volta, lista_de_erros):
     """ Verifica se a data de ida é maior do que a de volta """
     if data_ida > data_volta:
-        lista_de_erros['volta'] = 'Data de ida não pode ser maior do que data de volta'
+        lista_de_erros['data_volta'] = 'Data de ida não pode ser maior do que data de volta'
         
 def data_de_ida_menor_que_hoje(data_ida, data_pesquisa, lista_de_erros):
     """ Verifica se a data de pesquisa é menor que hoje """
     if data_ida < data_pesquisa:
-        lista_de_erros['ida'] = 'Data de ida não pode ser menor que hoje'
+        lista_de_erros['data_ida'] = 'Data de ida não pode ser menor que hoje'
--- apps/passagens/views/passagens.py
+++ apps/passagens/views/passagens.py
@@ -4,22 +4,24 @@ index - Página com formulário da passagem
 minha_consulta - Página que exibe os dados do formulário
 """
 from django.shortcuts import render
-from passagens.forms import PassagemForms
+from passagens.forms import PassagemForms,PessoaForms
 
 
 def index(request):
     """ Configura a view index """
     form = PassagemForms()
-    contexto = {'form':form}
+    pessoa_form = PessoaForms()
+    contexto = {'form':form, 'pessoa_form':pessoa_form}
     return render(request, 'index.html', contexto)
 
 def minha_consulta(request):
     """ Exibe os dados do formulários """
     if request.method == 'POST':
         form = PassagemForms(request.POST)
-        contexto = {'form':form}
+        pessoa_form = PessoaForms(request.POST)
+        contexto = {'form':form, 'pessoa_form':pessoa_form}
         if form.is_valid():
             return render(request, 'minha_consulta.html', contexto)
         return render(request, 'index.html', contexto)
-    contexto = {'form':form}
+    contexto = {'form':form, 'pessoa_form':pessoa_form}
     return render(request, 'index.html', contexto)
